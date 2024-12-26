1dabf265c443c109c92c901519af7d6e63824fd2
Kai Kameyama <1203kk06@gmail.com>
2024-12-27 07:07:45 +0900

    final product with bonus challenge

--- script.js
+++ script.js
@@ -2,40 +2,52 @@ const container = document.getElementById('container');
 const gridBtn = document.getElementById('gridBtn');
 const rbwBtn = document.getElementById('rbwBtn');
 
-function startEtching(size){
+let rnwMode = false;
+
+function startEtching(size = 16) {
+
     function makeGrid() {
-        for (i = 0; i < size; i++) {
-          for (j = 0; j < size; j++) {
-            const row = document.createElement("div");
-            container.appendChild(row);
-            row.classList.add("grid-style", "grids");
-            row.style.width = `${700/size}px`;
-            row.style.height = `${700/size}px`;
-          }
+        container.innerHTML = ""; 
+        for (let i = 0; i < size; i++) {
+            for (let j = 0; j < size; j++) {
+                const row = document.createElement("div");
+                container.appendChild(row);
+                row.classList.add("grid-style", "grids");
+                row.style.width = `${700 / size}px`;
+                row.style.height = `${700 / size}px`;
+            }
         }
     }
-    
-    function hover(){
+
+
+    function hover() {
         const allGrids = document.querySelectorAll('.grids');
         allGrids.forEach((grid) => {
             grid.addEventListener("mouseover", () => {
-                grid.style.backgroundColor = "black";
-            })
-        })
+                if (!rnwMode) {
+                    grid.style.backgroundColor = "black"; 
+                } else {
+                    const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+                    const r = randomBetween(0, 255);
+                    const g = randomBetween(0, 255);
+                    const b = randomBetween(0, 255);
+                    grid.style.backgroundColor = `rgb(${r},${g},${b})`; 
+                }
+            });
+        });
     }
-
+    
     makeGrid();
     hover();
 }
 
-function createNewGrid(){
+function createNewGrid() {
     const size = Number(prompt("Please enter a number lower than 100"));
-    if (size > 100 || size < 1){
+    if (size > 100 || size < 1) {
         alert("Enter A Valid Input!");
         return;
     }
 
-    container.innerHTML = "";
     startEtching(size);
 }
 
@@ -43,3 +55,14 @@ function createNewGrid(){
 startEtching(16);
 
 gridBtn.addEventListener('click', () => createNewGrid());
+
+rbwBtn.addEventListener('click', () => {
+    rnwMode = !rnwMode; 
+    rbwBtn.textContent = rnwMode ? "Rainbow Mode: ON" : "Rainbow Mode: OFF";
+    
+
+    const allGrids = document.querySelectorAll('.grids');
+    allGrids.forEach(grid => {
+        grid.style.backgroundColor = ""; 
+    });
+});
